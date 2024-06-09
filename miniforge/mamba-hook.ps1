f70cd4e55bbeeee668eab08aa9d45fe928022e80
Dogukan <dogukan.mertoglu01@gmail.com>
2024-06-09 14:29:36 +0200

    added shortcut command and genral path

--- miniforge/mamba-hook.ps1
+++ miniforge/mamba-hook.ps1
@@ -1,9 +1,9 @@
-$Env:CONDA_EXE = "C:\Users\Dogukan\miniforge3\Scripts\conda.exe"
+$Env:CONDA_EXE = "path\to\miniforge3\Scripts\conda.exe"
 $Env:_CE_M = ""
 $Env:_CE_CONDA = ""
-$Env:_CONDA_ROOT = "C:\Users\Dogukan\miniforge3"
-$Env:_CONDA_EXE = "C:\Users\Dogukan\miniforge3\Scripts\conda.exe"
-$Env:MAMBA_EXE = "C:\Users\Dogukan\miniforge3\Scripts\mamba.exe"
+$Env:_CONDA_ROOT = "path\to\miniforge3"
+$Env:_CONDA_EXE = "path\to\miniforge3\Scripts\conda.exe"
+$Env:MAMBA_EXE = "path\to\miniforge3\Scripts\mamba.exe"
 $CondaModuleArgs = @{ChangePs1 = $True}
 Import-Module "$Env:_CONDA_ROOT\shell\condabin\Mamba.psm1" -ArgumentList $CondaModuleArgs
 
