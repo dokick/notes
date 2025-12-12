a10d8549b70a9cf26ec6b5af3ede0e38e6d89797
Dogukan Mertoglu <dogukan.mertoglu01@gmail.com>
2025-12-12 21:33:46 +0100

    fix: update conda/mamba scripts

--- miniforge/mamba-hook.ps1
+++ miniforge/mamba-hook.ps1
@@ -1,9 +1,10 @@
-$Env:CONDA_EXE = "path\to\miniforge3\Scripts\conda.exe"
-$Env:_CE_M = ""
-$Env:_CE_CONDA = ""
+$Env:CONDA_EXE = "path/to/miniforge3\Scripts\conda.exe"
+$Env:_CONDA_EXE = "path/to/miniforge3\Scripts\conda.exe"
+$Env:_CE_M = $null
+$Env:_CE_CONDA = $null
+$Env:CONDA_PYTHON_EXE = "path/to/miniforge3\python.exe"
 $Env:_CONDA_ROOT = "path\to\miniforge3"
-$Env:_CONDA_EXE = "path\to\miniforge3\Scripts\conda.exe"
-$Env:MAMBA_EXE = "path\to\miniforge3\Scripts\mamba.exe"
+$Env:MAMBA_EXE = "path/to/miniforge3\Library\bin\mamba.exe"
 $CondaModuleArgs = @{ChangePs1 = $True}
 Import-Module "$Env:_CONDA_ROOT\shell\condabin\Mamba.psm1" -ArgumentList $CondaModuleArgs
 
